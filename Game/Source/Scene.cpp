fef0c1825c78e10bd5c0cd525e7e3723fc06b709
DESKTOP-C8H0847\Ramsubo <marctripleh@gmail.com>
2020-11-01 23:26:45 +0100

    Keys Save&Load Changed

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -50,10 +50,10 @@ bool Scene::PreUpdate()
 bool Scene::Update(float dt)
 {
     // L02: DONE 3: Request Load / Save when pressing L/S
-	if(app->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
+	if(app->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
 		app->LoadGameRequest();
 
-	if(app->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
+	if(app->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
 		app->SaveGameRequest();
 
 	if (app->player->collider->type == COLLIDER_GODMODE)
